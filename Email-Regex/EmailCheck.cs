70cc52bece74e069480c3d2ab20ed90317e19122
unknown <sandeepshinde630@gmail.com>
2021-08-19 17:52:00 +0530

    feat:Added code

---
+++ Email-Regex/EmailCheck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Email_Regex
+{
+    public class EmailCheck
+    {
+        public static string Email = "^[a-z]+([._+-]*[a-z0-9]+)*[@][a-z0-9]+[.][a-z]{2,3}(.[a-z]{2})?$";
+        public bool validateEmail(string input)
+        {
+            return Regex.IsMatch(input, Email);
+        }
+    }
+}
---
+++ Email-Regex/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Email_Regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Email Regex Demo!");
+            EmailCheck emailCheck = new EmailCheck();
+            bool result2 = emailCheck.validateEmail("sandeepshinde630@gmail.com");
+            Console.WriteLine(result2);
+        }
+    }
+}
